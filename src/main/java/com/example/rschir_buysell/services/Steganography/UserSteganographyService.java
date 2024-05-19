3e15252f6b8bac442a372ea427f14b29c1e4dd1b
BadHard101 <BadHard101@yandex.ru>
2024-05-19 04:02:08 +0300

    Заготовка на всю Стеганографию готов

--- src/main/java/com/example/rschir_buysell/services/Steganography/UserSteganographyService.java
+++ src/main/java/com/example/rschir_buysell/services/Steganography/UserSteganographyService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserSteganographyService {
     private final UserRepository userRepository;
+
     public boolean[] checkStegano_Level1(Answers answers, User user) {
         boolean[] result = {false, false, false, false, false};
         if (answers.getAnswer1().equals("стегосистема"))
@@ -22,12 +23,96 @@ public class UserSteganographyService {
             result[2] = true;
         if (answers.getAnswer4().equals("внедрения") || answers.getAnswer4().equals("алгоритм внедрения"))
             result[3] = true;
-            if (answers.getAnswer5().equals("форматирование"))
+        if (answers.getAnswer5().equals("форматирование"))
             result[4] = true;
 
         if (result[0] && result[1] && result[2] && result[3] && result[4]
                 && user.getSteganoProgress() <= 0) {
-            user.setSteganoProgress(user.getSteganoProgress()+1);
+            user.setSteganoProgress(user.getSteganoProgress() + 1);
+            userRepository.save(user);
+        }
+        return result;
+    }
+
+    public boolean[] checkStegano_Level2(Answers answers, User user) {
+        boolean[] result = {false, false, false, false, false};
+        if (answers.getAnswer1().equals("стегосистема"))
+            result[0] = true;
+        if (answers.getAnswer2().equals("1499"))
+            result[1] = true;
+        if (answers.getAnswer3().equals("волосами") || answers.getAnswer3().equals("волосы"))
+            result[2] = true;
+        if (answers.getAnswer4().equals("внедрения") || answers.getAnswer4().equals("алгоритм внедрения"))
+            result[3] = true;
+        if (answers.getAnswer5().equals("форматирование"))
+            result[4] = true;
+
+        if (result[0] && result[1] && result[2] && result[3] && result[4]
+                && user.getSteganoProgress() <= 1) {
+            user.setSteganoProgress(user.getSteganoProgress() + 1);
+            userRepository.save(user);
+        }
+        return result;
+    }
+
+    public boolean[] checkStegano_Level3(Answers answers, User user) {
+        boolean[] result = {false, false, false, false, false};
+        if (answers.getAnswer1().equals("стегосистема"))
+            result[0] = true;
+        if (answers.getAnswer2().equals("1499"))
+            result[1] = true;
+        if (answers.getAnswer3().equals("волосами") || answers.getAnswer3().equals("волосы"))
+            result[2] = true;
+        if (answers.getAnswer4().equals("внедрения") || answers.getAnswer4().equals("алгоритм внедрения"))
+            result[3] = true;
+        if (answers.getAnswer5().equals("форматирование"))
+            result[4] = true;
+
+        if (result[0] && result[1] && result[2] && result[3] && result[4]
+                && user.getSteganoProgress() <= 2) {
+            user.setSteganoProgress(user.getSteganoProgress() + 1);
+            userRepository.save(user);
+        }
+        return result;
+    }
+
+    public boolean[] checkStegano_Level4(Answers answers, User user) {
+        boolean[] result = {false, false, false, false, false};
+        if (answers.getAnswer1().equals("стегосистема"))
+            result[0] = true;
+        if (answers.getAnswer2().equals("1499"))
+            result[1] = true;
+        if (answers.getAnswer3().equals("волосами") || answers.getAnswer3().equals("волосы"))
+            result[2] = true;
+        if (answers.getAnswer4().equals("внедрения") || answers.getAnswer4().equals("алгоритм внедрения"))
+            result[3] = true;
+        if (answers.getAnswer5().equals("форматирование"))
+            result[4] = true;
+
+        if (result[0] && result[1] && result[2] && result[3] && result[4]
+                && user.getSteganoProgress() <= 3) {
+            user.setSteganoProgress(user.getSteganoProgress() + 1);
+            userRepository.save(user);
+        }
+        return result;
+    }
+
+    public boolean[] checkStegano_Level5(Answers answers, User user) {
+        boolean[] result = {false, false, false, false, false};
+        if (answers.getAnswer1().equals("стегосистема"))
+            result[0] = true;
+        if (answers.getAnswer2().equals("1499"))
+            result[1] = true;
+        if (answers.getAnswer3().equals("волосами") || answers.getAnswer3().equals("волосы"))
+            result[2] = true;
+        if (answers.getAnswer4().equals("внедрения") || answers.getAnswer4().equals("алгоритм внедрения"))
+            result[3] = true;
+        if (answers.getAnswer5().equals("форматирование"))
+            result[4] = true;
+
+        if (result[0] && result[1] && result[2] && result[3] && result[4]
+                && user.getSteganoProgress() <= 4) {
+            user.setSteganoProgress(user.getSteganoProgress() + 1);
             userRepository.save(user);
         }
         return result;
