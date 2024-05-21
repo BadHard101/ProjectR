db5795abec9f8c4996a34208f62350f35a021327
BadHard101 <BadHard101@yandex.ru>
2024-05-21 20:50:40 +0300

    Level 2 -> Level 6

--- src/main/java/com/example/rschir_buysell/services/Cryptanalysis/Level6/UserCryptanalysisLevel6Service.java
+++ src/main/java/com/example/rschir_buysell/services/Cryptanalysis/Level6/UserCryptanalysisLevel6Service.java
@@ -1,4 +1,4 @@
-package com.example.rschir_buysell.services.Cryptanalysis.Level2;
+package com.example.rschir_buysell.services.Cryptanalysis.Level6;
 
 import com.example.rschir_buysell.models.Answers;
 import com.example.rschir_buysell.models.User;
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class UserCryptanalysisLevel2Service {
+public class UserCryptanalysisLevel6Service {
     private final UserRepository userRepository;
 
     /**
      * Theory
      */
-    public void check_Level2_theory(Answers answers, User user) { // CHANGE
+    public void check_Level6_theory(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 5) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -27,7 +27,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 0
      */
-    public void check_Level2_practice0(Answers answers, User user) { // CHANGE
+    public void check_Level6_practice0(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 6) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -38,7 +38,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 1
      */
-    public boolean[] check_Level2_practice1(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_practice1(Answers answers, User user) { // CHANGE
         boolean[] result = {false};
         if (answers.getAnswer1().equals("dabccadbbdac")) {
             result[0] = true;
@@ -55,7 +55,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 2
      */
-    public boolean[] check_Level2_practice2(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_practice2(Answers answers, User user) { // CHANGE
         boolean[] result = {false};
         if (answers.getAnswer1().equals("4,405")) {
             result[0] = true;
@@ -72,7 +72,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 3
      */
-    public boolean[] check_Level2_practice3(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_practice3(Answers answers, User user) { // CHANGE
         boolean[] result = {false};
         if (answers.getAnswer1().equals("bziperjvofhluayxncdmtwkgsq")) {
             result[0] = true;
@@ -89,7 +89,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 4
      */
-    public boolean[] check_Level2_practice4(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_practice4(Answers answers, User user) { // CHANGE
         boolean[] result = {false, false};
         if (answers.getAnswer1().equals("24"))
             result[0] = true;
@@ -107,7 +107,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 5
      */
-    public void check_Level2_practice5(Answers answers, User user) { // CHANGE
+    public void check_Level6_practice5(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 11) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -117,7 +117,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 6
      */
-    public void check_Level2_practice6(Answers answers, User user) { // CHANGE
+    public void check_Level6_practice6(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 12) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -127,7 +127,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 7
      */
-    public void check_Level2_practice7(Answers answers, User user) { // CHANGE
+    public void check_Level6_practice7(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 13) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -137,7 +137,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 8
      */
-    public void check_Level2_practice8(Answers answers, User user) { // CHANGE
+    public void check_Level6_practice8(Answers answers, User user) { // CHANGE
         if (user.getCryptoProgress() <= 14) { // CHANGE
             user.setCryptoProgress(user.getCryptoProgress()+1);
             userRepository.save(user);
@@ -147,7 +147,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Practice 9
      */
-    public boolean[] check_Level2_practice9(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_practice9(Answers answers, User user) { // CHANGE
         boolean[] result = {false, false, false};
         if (answers.getAnswer1().equals("znufbamhegxvdprtkojiywcslq"))
             result[0] = true;
@@ -165,7 +165,7 @@ public class UserCryptanalysisLevel2Service {
     /**
      * Control
      */
-    public boolean[] check_Level2_control(Answers answers, User user) { // CHANGE
+    public boolean[] check_Level6_control(Answers answers, User user) { // CHANGE
         boolean[] result = {false, false, false};
         if (answers.getAnswer1().equals("baxwzwxgaeipunfimmasw"))
             result[0] = true;
